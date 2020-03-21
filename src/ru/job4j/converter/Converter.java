781fb6b46975c599c1895805a5240cac4ff3c2c4
artyomzhoy <artyomzhoy@gmail.com>
2020-03-21 19:27:39 +0300

    4.1. Результат работы метода.[#257694]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,38 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static double rubleToEuro (double value) {
+        double rsl = value / 70;
+        return  rsl;
+    }
+    public static double rubleToDollar (double value) {
+        double rsl = value / 60;
+        return rsl;
+    }
+    public static double euroToRuble (double value) {
+        double rsl = value * 70;
+        return rsl;
+    }
+    public static double dollarToRuble (double value) {
+        double rsl = value * 60;
+        return rsl;
+    }
+
+    public static void main (String[] args) {
+
+        int ruble = 571; // ДАНО
+
+        double euroRuble = rubleToEuro(ruble);
+        System.out.println (ruble + " rubles are " + euroRuble + " euro.");
+
+        double dollarRuble = rubleToDollar(ruble);
+        System.out.println (ruble + " rubles are " + dollarRuble + " dollar.");
+
+        double rubleEuro = rubleToEuro(euroToRuble(ruble));
+        System.out.println (euroRuble + " euro are " + rubleEuro + " rubles.");
+
+        double rubleDollar = rubleToDollar(dollarToRuble(ruble));
+        System.out.println (dollarRuble + " dollar are " + rubleDollar + " rubles.");
+    }
+}
