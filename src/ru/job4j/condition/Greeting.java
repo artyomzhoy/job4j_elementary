08ade582227605752ee4b6b2479b8f9497ea51fa
artyomzhoy <artyomzhoy@gmail.com>
2020-03-22 22:55:51 +0300

    9.1. String[#257695] fixes

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,10 +4,9 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        String newbie = "But I am a newbie.";
-        String separate = " ";
+        idea = idea + "But I am a newbie.";
         int year = 2020;
-        idea = idea + separate + newbie + separate + year;
+        idea = idea + year;
         System.out.println(idea);
     }
 }
